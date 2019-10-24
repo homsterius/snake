ece5244d8ff8fc6ec76b9d716ec72ff907edaa47
Pavel Rozhkov <homsterius@gmail.com>
2019-10-24 22:30:23 +0300

    added classes controlling the game

---
+++ src/main/java/snake/ControllerEvent.java
@@ -0,0 +1,8 @@
+package snake;
+
+enum ControllerEvent {
+    KEY_UP,
+    KEY_DOWN,
+    KEY_RIGHT,
+    KEY_LEFT,
+}
---
+++ src/main/java/snake/ControllerListener.java
@@ -0,0 +1,6 @@
+package snake;
+
+public interface ControllerListener {
+
+    void update(ControllerEvent controllerEvent);
+}
