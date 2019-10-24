f431ada17acf5c0b23d75b4dc0ac7c61ed37aecc
Pavel Rozhkov <homsterius@gmail.com>
2019-10-24 20:02:03 +0300

    class BoardFactory to simplify making objects of Board class

---
+++ src/main/java/snake/BoardFactory.java
@@ -0,0 +1,15 @@
+package snake;
+
+class BoardFactory {
+
+    static Board buildSquareBoard(int size) {
+        var points = new Point[4];
+
+        points[0] = new Point(0,0);
+        points[1] = new Point(0, size);
+        points[2] = new Point(size, size);
+        points[3] = new Point(size, 0);
+
+        return new Board(points);
+    }
+}
